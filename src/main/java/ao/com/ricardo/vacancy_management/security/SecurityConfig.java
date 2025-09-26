4d94612811cd31f52ef9a35e2ecf9aa58eb22e6a
Ricardo João António <ricardojoaoantonio2@gmail.com>
2025-09-26 16:50:35 +0100

    ✨ feat: Configuring Public Routes

--- src/main/java/ao/com/ricardo/vacancy_management/security/SecurityConfig.java
+++ src/main/java/ao/com/ricardo/vacancy_management/security/SecurityConfig.java
@@ -10,7 +10,13 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable());
+		http.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> {
+				auth.requestMatchers("/candidate/").permitAll();
+						auth.requestMatchers("/company/").permitAll();
+				auth.anyRequest().authenticated();
+			})
+		;
 		return http.build();
 	}
 }
