814a396a43c39475f646d8998ee99c880c813251
Ricardo João António <ricardojoaoantonio2@gmail.com>
2026-06-20 02:42:14 +0100

    JSB-2 Configure Swagger

--- src/main/java/ao/com/ricardo/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/ao/com/ricardo/vacancy_management/VacancyManagementApplication.java
@@ -3,7 +3,17 @@ package ao.com.ricardo.vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Vacancy Management API",
+		version = "1.0",
+		description = "API for managing job vacancies"
+	)
+)
 public class VacancyManagementApplication {
 
 	public static void main(String[] args) {
